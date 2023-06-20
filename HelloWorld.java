f36d7201e6aaa42468e7ee2945ad4d32c8993a6f
ozden3550 <whiteangel1805@hotmail.com>
2023-06-20 22:50:43 +0200

    chagned messged from HeklloWord.java class

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,11 @@ public class HelloWorld {
 
 
         System.out.println("Hello World");
-        System.out.println("How are you?");
+        System.out.println("Hi tahnak?");
         System.out.println();
+        System.out.println("Adem");
+        System.out.println("bade");
+        System.out.println("ozden");
+
     }
 }
