b0a1a28b6eeaeb7e65a32c545dda4fc34a43c122
ozden3550 <whiteangel1805@hotmail.com>
2023-06-20 22:44:29 +0200

    first commit from ozden

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package dayo1_introToProgrammimg;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+
+
+
+        System.out.println("Hello World");
+        System.out.println("How are you?");
+        System.out.println();
+    }
+}
