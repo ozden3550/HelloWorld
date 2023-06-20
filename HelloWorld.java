26dc5e7f9a6eee259fa3781f7396f9bda5505c52
ozden3550 <whiteangel1805@hotmail.com>
2023-06-20 22:51:06 +0200

    changed mesut data

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,6 +13,7 @@ public class HelloWorld {
         System.out.println("Adem");
         System.out.println("bade");
         System.out.println("ozden");
+        System.out.println("Mesut");
 
     }
 }
